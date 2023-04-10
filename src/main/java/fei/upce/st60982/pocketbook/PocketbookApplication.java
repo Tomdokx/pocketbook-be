8c9c5f8eacb366c4be83a1abe75e1c9fca80e826
Tomdok <tomdokx@gmail.com>
2023-04-10 19:37:01 +0200

    Init commit

---
+++ src/main/java/fei/upce/st60982/pocketbook/PocketbookApplication.java
@@ -0,0 +1,13 @@
+package fei.upce.st60982.pocketbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fei/upce/st60982/pocketbook/PocketbookApplicationTests.java
@@ -0,0 +1,13 @@
+package fei.upce.st60982.pocketbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
